9839bf096b59c3e443baaba278f8d189f55d8725
Sudheer Kumar <sudheermsdvk@gmail.com>
2025-03-25 18:31:30 +0530

    Update assembly.py

--- assembly.py
+++ assembly.py
@@ -1,6 +1,6 @@
 import assemblyai as aai
 
-aai.settings.api_key = "f256a97fbb8944859bef065dc1e0290f"
+aai.settings.api_key = ""
 transcriber = aai.Transcriber()
 
 transcript = transcriber.transcribe("https://assembly.ai/news.mp4")
